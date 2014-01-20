36546ce2757beeb6314539d6b69c3720ad2ab731
Liam Kelly <liam_kelly@oxfordcorp.com>
2014-01-20 15:11:26 -0500

    second take, still pre-deployment

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,13 +1,40 @@
-
+// ON DOCUMENT READY
 $().ready( function() {
 
     $('map area').click( function(evt) {
         var opt = $(this).attr('title');
         $('.pagecontent').hide();
-        $('#navmap').attr('src','assets/images/WebsiteMenu'+opt.charAt(0).toUpperCase() + opt.slice(1)+'.jpg');
+        $('#map').attr('src','assets/images/WebsiteMenu'+opt.charAt(0).toUpperCase() + opt.slice(1)+'.jpg');
         $('#'+opt).show();
-        evt.preventDefault()
+        evt.preventDefault();
+        // TODO: fix these relative paths for production
+        if( opt === 'event_info' ) {
+            $('#content_area').css('background-image','url(../gallatin/assets/images/AlbertGallatinContentLogosNCopy.jpg)');
+        } else {
+            $('#content_area').css('background-image','url(../gallatin/assets/images/AlbertGallatinContent.jpg)');
+        }
     })
 
+    $('#register_btn').click( function(evt) {
+        $('#form form').ajaxSubmit({
+            success : function(r) {
+                alert('Success');
+            }
+        })
+    })
+
+    // EVENT HANDLERS
+    $(window).bind("resize", listenWidth)
+
+    $('<div/>').css('background-image','url(../gallatin/assets/images/AlbertGallatinContent.jpg)');
 
 })
+
+// FUNCTIONS
+function listenWidth( e ) {
+    if($(window).width()>320) {
+        $("#about_area").remove().insertAfter($("#content_area"));
+    } else {
+        $("#about_area").remove().insertBefore($("#content_area"));
+    }
+}
